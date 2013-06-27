370bcb76744226dc6d2ffc98c32fefdedc1a7989
omar delarosa <thedelarosa@gmail.com>
2013-06-27 10:44:17 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylie::Application.config.secret_token = '230d46b0ce5361b7c4214e6e8fa23f83108f340f2ca7b74e538e4b5bb4ce3cea46217b528d7b1aa8f14e9776bdf0e18f50d0d8ef6026c25405e4eaabdee5f09f'
